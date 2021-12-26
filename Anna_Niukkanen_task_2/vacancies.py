eafc1d68180c09aae08c5975046b25ec96e2e2d6
Annushka <aniukkan@gmail.com>
2021-12-27 01:28:25 +0200

    Task_2 is already ready

--- Anna_Niukkanen_task_2/vacancies.py
+++ Anna_Niukkanen_task_2/vacancies.py
@@ -10,18 +10,37 @@ params = {'clusters': 'true',
           'ored_clusters': 'true',
           'enable_snippets': 'true',
           'salary': '0',
-          'text': 'data+engineer'
+          'text': 'data engineer'
           }
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'}
 
 response = requests.get(main_url + '/search/vacancy', params=params, headers=headers)
+# pprint(response.text)
+
 
 if response.ok:
     dom = bs(response.text, 'html.parser')
     vacancies = dom.find_all('div', {'class': 'vacancy-serp-item'})
 
+    vacancy_list = []
+
     for vacancy in vacancies:
-        name = vacancy.find('a', {'class': 'bloko-link'})
-        print()
+        vacancy_data = {}
+        info = vacancy.find('a', {'class': 'bloko-link'})
+        name = info.text
+        link = info.get('href')
+        salary_min = vacancy.find('span', {'class': 'bloko-header-section-3'}).text
+        try:
+            salary_min = float(salary_min)
+        except:
+            salary_min = None
+
+        vacancy_data['name'] = name
+        vacancy_data['link'] = link
+        vacancy_data['salary_min'] = salary_min
+
+        vacancy_list.append(vacancy_data)
+pprint(vacancy_list)
 
